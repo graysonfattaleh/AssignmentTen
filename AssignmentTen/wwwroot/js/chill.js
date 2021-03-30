880e8fdcd6ca75e0516120e93bcd1cdfc5560bf0
graysonfattaleh <71087950+graysonfattaleh@users.noreply.github.com>
2021-03-30 14:37:25 -0600

    Better Styling

--- AssignmentTen/wwwroot/js/chill.js
+++ AssignmentTen/wwwroot/js/chill.js
@@ -4,9 +4,9 @@ function changeLink() {
     let active_link = document.getElementById("current_cat").innerHTML;
     let link_key = "link-" + active_link
 
-    document.getElementById(link_key).style.color = "red";
+    document.getElementById(link_key).style.backgroundColor = "rgba(171, 183, 183, 0.9)";
 
-    hot_link.style.color = "red";
+   
 
 }
 
